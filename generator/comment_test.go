68f583435485303b5a921a7824b9b31541ea5a51
moznion <moznion@gmail.com>
2019-01-16 23:19:48 +0900

    Support `NewCommentf`: `NewComment` with `Sprintf`

--- generator/comment_test.go
+++ generator/comment_test.go
@@ -21,3 +21,10 @@ func TestShouldGenerateCommentStatement(t *testing.T) {
 		assert.Equal(t, "\t\t// this is a comment\n", gen)
 	}
 }
+
+func TestShouldGenerateCommentStatementWithFormatting(t *testing.T) {
+	generator := NewCommentf("this is a %s", "comment")
+	gen, err := generator.Generate(0)
+	assert.NoError(t, err)
+	assert.Equal(t, "//this is a comment\n", gen)
+}
